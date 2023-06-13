18e9d652153b7f35bed489909ac56f503f3c9549
Zakaria KASSRAOUI <zakaria.kassraoui@scafe.io>
2023-06-13 14:39:35 +0200

    SCAFE-70 init ACR Module

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Premium"
+  admin_enabled       = true
+}
+
+resource "azurerm_private_endpoint" "acr" {
+  name                = "acr-endpoint"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "acr-priv"
+    private_connection_resource_id = azurerm_container_registry.acr.id
+    is_manual_connection           = false
+    subresource_names              = ["registry"]
+  }
+}
